8d2351073626233de8e2a2a9be8f794abe1bba1d
bisworanjanswain4u <bisworanjanswain4u@gmail.com>
2026-09-16 17:51:36 +0530

    Update chintu.java

--- chintu.java
+++ chintu.java
@@ -1,12 +1,12 @@
 class Test{
     public static void main(String[] args){
         System.out.println("Hello World");
-<<<<<<< HEAD
+
         int a =10;
-=======
+
         System.out.println(
             "This commit is done by sachin"
         );
->>>>>>> 6c16b80d77f9bab84b2e91ebdb24c61db076811a
+
     }
 }
